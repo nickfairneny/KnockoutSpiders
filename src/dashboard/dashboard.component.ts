e0ed05b09849b3088f071bb407c31971cfc41d05
arielcamargo <70982774+arielcamargo@users.noreply.github.com>
2022-06-09 16:49:15 -0700

    Fixing path errors of Quan's work.

--- src/dashboard/dashboard.component.ts
+++ src/dashboard/dashboard.component.ts
@@ -2,7 +2,9 @@ import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  templateUrl: '/dashboard.component.html'
+  templateUrl: '/dashboard.component.html',
+  styleUrls: ['/dashboard.component.css', '../assets/fonts/fontawesome-free-6.1.1-web/css/all.min.css']
+
 })
 
 export class DashboardComponent implements OnInit
