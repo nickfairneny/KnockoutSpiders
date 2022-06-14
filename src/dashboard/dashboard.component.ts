c7abc635930e2863d568b85bec7c01e6ed163778
arielcamargo <70982774+arielcamargo@users.noreply.github.com>
2022-06-13 23:10:36 -0700

    Added getStandardDeviation and getAverage which when given a array, it will calculate the respected values.

--- src/dashboard/dashboard.component.ts
+++ src/dashboard/dashboard.component.ts
@@ -12,4 +12,32 @@ export class DashboardComponent implements OnInit
   ngOnInit()
   {
   }
+
+  //Author: Ariel Camargo
+  //Given an array, the standard deviation is returned.
+  getStandardDeviation(data:number[])
+  {
+    let average = this.getAverage(data);
+    let standardDeviation = 0.0;
+    for(let i = 0; i < data.length; i++)
+    {
+      standardDeviation += Math.pow((data[i] - average), 2);
+    }
+    standardDeviation /= data.length;
+    standardDeviation = Math.sqrt(standardDeviation);
+    return standardDeviation;
+  }
+
+  //Author: Ariel Camargo
+  //Given an array, the average is returned.
+  getAverage(data:number[])
+  {
+    let average = 0.0;
+    for(let i = 0; i < data.length; i++)
+    {
+      average += data[i];
+    }
+    average /= data.length;
+    return average;
+  }
 }
